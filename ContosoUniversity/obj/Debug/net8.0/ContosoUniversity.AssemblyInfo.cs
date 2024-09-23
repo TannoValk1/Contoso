80cd9f061e615ff954f7bf350ecca906699b383f
Your Name <youremail@yourdomain.com>
2024-09-23 13:48:49 +0300

    Tegime Departmenti ja Departmentile Indexi

--- ContosoUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ccf1a52ba72038ce9cd5b5438fc72194399c83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a886e98855ffd38e5b59117bfdf79678d43bcbdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
