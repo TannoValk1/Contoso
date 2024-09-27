98639d3612be651ce6eaab73aab8bca47830f123
Opilane TTHK <opilane@tthk.ee>
2024-09-27 16:53:28 +0300

    Delete nupp töötab

--- ContosoUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+027242da9fdb5ebd71973be4b7287d4cd687bf3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1697b1561bfd13c733abbf67dc987492da35d8d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
